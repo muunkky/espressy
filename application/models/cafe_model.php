2038303b8781e81bc6ef426527ed30bd3da2928c
muunkky <cameronrout@gmail.com>
2015-06-07 06:50:14 +0000

    output cities and position

--- application/models/cafe_model.php
+++ application/models/cafe_model.php
@@ -119,6 +119,8 @@ public function list_cities_by_distance($lat1,$lon1){
     $city_dist = array();
 	$cities = $this->rise_cafe->ListRegion(0);
   	for($i=0;$i<count($cities);$i++){
+  	  echo "Lat: ".$lat1." Long: ".$lon1."<br />";
+  	  print_r("<pre>".$cities[&i]."</pre>");
   		if($lat1*$lon1!=0){
 	        $lat2 = $cities[$i]->Center_Latitude;
 	        $lon2 = $cities[$i]->Center_Longitude;
