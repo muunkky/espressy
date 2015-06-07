7cb68e69d7b66dd2187c95c4c5e46c1478d40ac7
muunkky <cameronrout@gmail.com>
2015-06-07 07:30:24 +0000

    add reviewer update form

--- application/views/list/review.php
+++ application/views/list/review.php
@@ -108,11 +108,12 @@ $threestar = "";
     });
 </script>
 
+</div>
+
+
 <div class="row collapse update-collapse-<?=$cafe_id?>" id="update-collapse-<?=$cafe_id?>" style="background:black;color:white;">
 <?php
       	$this->load->view("review/add_cafe",array("regions"=>$regions,"chains"=>$chains));
       ?>
     </div>
-</div>
-
 </div>
