ac4903de31d9bc2494f3bee0f45c7db1189ace82
muunkky <cameronrout@gmail.com>
2015-06-14 11:19:47 +0000

    gave more room to distance buttons

--- application/views/list/cafe.php
+++ application/views/list/cafe.php
@@ -31,8 +31,10 @@
     		</span>
   	    	<h5>
 
-  	    		<?=min(round($cafe->Distance,1),100)?><?php if($cafe->Distance>100){echo "+";}?> km
+  	    		
   	    		<?=$cafe->Hours?>
+  	    		,&nbsp;
+  	    		<?=min(round($cafe->Distance,1),100)?><?php if($cafe->Distance>100){echo "+";}?> km away
 				<?=($cafe->Star_Rating==0?"<h3 style='margin: 0;'>":"")?>
 		        	<a target="_blank" href="http://maps.google.com/?q=<?=$cafe->Name;?>,%20<?=$cafe->Address;?>" style="padding:3px;color:black;margin-right:15px;border:1px solid #DDD;border-radius:5px;background-color:transparent;" class="btn">
 		        		<span class="glyphicon glyphicon-globe"></span>
