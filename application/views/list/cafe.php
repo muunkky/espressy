33ee9b9103eb7ffbac083cbd9cc0e9b0a3bb942f
muunkky <cameronrout@gmail.com>
2015-06-14 11:01:43 +0000

    gave more room to distance buttons

--- application/views/list/cafe.php
+++ application/views/list/cafe.php
@@ -1,54 +1,47 @@
 <li class="list-group-item" style="<?=($cafe->Star_Rating==0?"padding-top: 0;padding-bottom: 0;":"")?>">
-    <div class="row">
-      <div class="col-xs-10">
-      	<?=($cafe->Star_Rating==0?"<h4>".$cafe->Name."</h4>":"<h2>".$cafe->Name."</h2>")?>
-  	    
-      	<h5><?=$cafe->Hours?>
-      		<?if(count($cafe->Sisters)>0){?>
-  	    	
-  			<a class="btn btn-xs" data-toggle="collapse" data-target="#<?=$cafe->ID?>_sisters" style="padding-bottom:3px;color:black;margin-top:-5px;border:1px solid #DDD;border-radius:5px;background-color:transparent;">+<?=(count($cafe->Sisters))?> Cafes</a>
-  		<?php } ?>
-      	</h5>
-      </div>
-      <div class="col-xs-2 text-center">
-        
-        <div class="row">
-        	<div class="col-xs-12">
-            	<h3 style="<?=($cafe->Star_Rating==0?"margin: 0;":"")?>">
-	            	<a target="_blank" href="http://maps.google.com/?q=<?=$cafe->Name;?>,%20<?=$cafe->Address;?>" style="padding:3px;color:black;margin-right:15px;border:1px solid #DDD;border-radius:5px;background-color:transparent;" class="btn">
-	            		<span class="glyphicon glyphicon-globe"></span> <?=min(round($cafe->Distance,1),100)?><?php if($cafe->Distance>100){echo "+";}?> km
-                	</a>
-            	</h3>
-            	<p style="color:<?=($reviewed==true?'#868A08':'#DDD')?>;">
-      				<?php
-      					switch ($cafe->Star_Rating) {
-							  case 3: ?>
-								  <span class="glyphicon glyphicon-star"></span>
-								  <span class="glyphicon glyphicon-star"></span>
-								  <span class="glyphicon glyphicon-star"></span>
-								  <?php break;
-							  case 2: ?>
-								  <span class="glyphicon glyphicon-star"></span>
-								  <span class="glyphicon glyphicon-star"></span>
-								  <?php break; 
-							  case 1: ?>
-								  <span class="glyphicon glyphicon-star"></span>
-								  <?php break;
-							  case 0: ?>
-								  <!--<span class="label label-default" style="background-color: #DDD">Honourable Mention</span>-->
-								  <?php break;
-							  case -1: ?>
-								  <span class="glyphicon glyphicon-star-empty"></span>
-								  <?php break;
-							  default:
-								  
-								  break;
-						  }
-      				?>
-            	</p>
-        	</div>
+	<div class="row">
+		<div class="col-xs-10">
+      		<?=($cafe->Star_Rating==0?"<h4>".$cafe->Name."</h4>":"<h2>".$cafe->Name."</h2>")?>
+  	    	<h5><?=$cafe->Hours?>
+      			<?if(count($cafe->Sisters)>0){?>
+  					<a class="btn btn-xs" data-toggle="collapse" data-target="#<?=$cafe->ID?>_sisters" style="padding-bottom:3px;color:black;margin-top:-5px;border:1px solid #DDD;border-radius:5px;background-color:transparent;">+<?=(count($cafe->Sisters))?> Cafes</a>
+	  			<?php } ?>
+    	  	</h5>
+    	</div>
+    	<div class="col-xs-2 text-center">
+	    	<?=($cafe->Star_Rating==0?"<h3 style='margin: 0;'>"?>
+	        	<a target="_blank" href="http://maps.google.com/?q=<?=$cafe->Name;?>,%20<?=$cafe->Address;?>" style="padding:3px;color:black;margin-right:15px;border:1px solid #DDD;border-radius:5px;background-color:transparent;" class="btn">
+	        		<span class="glyphicon glyphicon-globe"></span> <?=min(round($cafe->Distance,1),100)?><?php if($cafe->Distance>100){echo "+";}?> km
+	        	</a>
+	    	<?=($cafe->Star_Rating==0?"</h3>"?>
+    		<p style="color:<?=($reviewed==true?'#868A08':'#DDD')?>;">
+	  			<?php
+	  				switch ($cafe->Star_Rating) {
+						  case 3: ?>
+							  <span class="glyphicon glyphicon-star"></span>
+							  <span class="glyphicon glyphicon-star"></span>
+							  <span class="glyphicon glyphicon-star"></span>
+							  <?php break;
+						  case 2: ?>
+							  <span class="glyphicon glyphicon-star"></span>
+							  <span class="glyphicon glyphicon-star"></span>
+							  <?php break; 
+						  case 1: ?>
+							  <span class="glyphicon glyphicon-star"></span>
+							  <?php break;
+						  case 0: ?>
+							  <!--<span class="label label-default" style="background-color: #DDD">Honourable Mention</span>-->
+							  <?php break;
+						  case -1: ?>
+							  <span class="glyphicon glyphicon-star-empty"></span>
+							  <?php break;
+						  default:
+							  
+							  break;
+					  }
+	  			?>
+    		</p>
         </div>
-      </div>
     </div>
     
    
