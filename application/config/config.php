b9ddf758a3b57a8be5f65488479f80b99cf5f8de
muunkky <cameronrout@gmail.com>
2015-06-14 09:13:09 +0000

    changed base_url to https://hidden-garden-9030.herokuapp.com

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://espressy.com/';
+$config['base_url']	= 'https://hidden-garden-9030.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
