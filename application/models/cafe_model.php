e0e76bbc2ffd588ad53d4f5b214af7462921f97d
muunkky <cameronrout@gmail.com>
2015-06-18 07:28:50 +0000

    inventing unconfirmed

--- application/models/cafe_model.php
+++ application/models/cafe_model.php
@@ -136,7 +136,7 @@ public function list_cities_by_distance($lat1,$lon1){
 
 		$cafes = $this->rise_cafe->ListCafeByRegion(0, $cities[$i]->ID);
 
-		$stars = array(-1=>0,0=>0,1=>0,2=>0,3=>0);
+		$stars = array(-2=>0,-1=>0,0=>0,1=>0,2=>0,3=>0);
 
 		foreach($cafes as $c){
 			$stars[$c->Star_Rating]=$stars[$c->Star_Rating]+1;
