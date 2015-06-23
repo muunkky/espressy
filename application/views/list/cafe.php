4116ff4748d1dad6f3f3a8d83f88a0da84aa0653
muunkky <cameronrout@gmail.com>
2015-06-23 20:49:17 +0000

    make the whole cafe a link to map

--- application/views/list/cafe.php
+++ application/views/list/cafe.php
@@ -30,7 +30,7 @@ if(!$hide){?>
 <a target="_blank" style="color:black;" href="http://maps.google.com/?q=<?=$cafe->Name;?>,%20<?=$cafe->Address;?>">
 <li class="list-group-item" style="<?=($cafe->Star_Rating==0?"padding-top: 0;padding-bottom: 0;":"")?>border:5px solid #DDD">
 	<div class="text-center">
-
+	<span class="pull-right" class="glyphicon glyphicon-map-marker"></span>
       		<?=($cafe->Star_Rating==0?"<h4>".$cafe->Name."</h4>":"<h2>".$cafe->Name."</h2>")?>
     		<span style="color:<?=($reviewed==true?'#868A08':'#DDD')?>;">
     			<?php if(!$reviewed){?>
@@ -68,7 +68,7 @@ if(!$hide){?>
   	    		
   	    		<?=$cafe->Hours?>
   	    		,&nbsp;
-  	    		<?=min(round($cafe->Distance,1),100)?><?php if($cafe->Distance>100){echo "+";}?> km away (open map)
+  	    		<?=min(round($cafe->Distance,1),100)?><?php if($cafe->Distance>100){echo "+";}?> km away
     	  	</h5>
     	  	
     	  	<h5>
