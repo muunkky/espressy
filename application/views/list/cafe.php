5b8e4a4e8a1a8e62932d6b0dd068ca31331a29ec
muunkky <cameronrout@gmail.com>
2015-08-16 22:04:32 +0000

    card view

--- application/views/list/cafe.php
+++ application/views/list/cafe.php
@@ -74,7 +74,7 @@ if(!$hide){?>
   	    	<h5>
   	    		<?=min(round($cafe->Distance,1),100)?><?php if($cafe->Distance>100){echo "+";}?> km away
   	    		<?php if(count($cafe->Sisters)>0){?>
-				  	<a class="btn btn-xs pull-right" data-toggle="collapse" data-target="#<?=$cafe->ID?>_sisters" style="color:black;border:1px solid #DDD;border-radius:5px;background-color:transparent;">
+				  	<a class="btn btn-xs" data-toggle="collapse" data-target="#<?=$cafe->ID?>_sisters" style="color:black;border:1px solid #DDD;border-radius:5px;background-color:transparent;">
 						<span data-toggle="tool-tip" title="Show <?=(count($cafe->Sisters))?> other <?=$cafe->Name?> cafes">+ <?=(count($cafe->Sisters))?></span>
 					</a>
 				<?php } ?>
